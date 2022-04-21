c3db9c93468865818008feafc15bfd0495e802dd
Le Van Hau <levanhau@Les-MacBook-Pro.local>
2022-04-21 16:11:07 +0700

    confix vertical scroll table

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,61 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
+import 'package:linked_scroll_controller/linked_scroll_controller.dart';
+
+import 'item_widget.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  double iconSize = 40;
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //double iconSize = 40;
+  LinkedScrollControllerGroup? _controllers;
+  ScrollController? _controller1;
+  ScrollController? _controller2;
+  ScrollController? _controller3;
+  ScrollController? _controller4;
+  ScrollController? _controller5;
+  @override
+  void initState() {
+    super.initState();
+    _controllers = LinkedScrollControllerGroup();
+    _controller1 = _controllers?.addAndGet();
+    _controller2 = _controllers?.addAndGet();
+    _controller3 = _controllers?.addAndGet();
+    _controller4 = _controllers?.addAndGet();
+    _controller5 = _controllers?.addAndGet();
+  }
+
+  @override
+  void dispose() {
+    _controller1?.dispose();
+    _controller2?.dispose();
+    _controller3?.dispose();
+    _controller4?.dispose();
+    _controller5?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,148 +64,246 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Flutter Table'),
         ),
-        body: Row(
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: 100,
-              height: 150,
-              child: Column(
-                children: [
-                  TableRowItem(width: 100, height: 50, title: '1 2'),
-                  // Table(
-                  //   children: [
-                  //     TableRow(
-                  //       children: [
-                  //         TableRowItem(width: 100, height: 50, title: '1 2'),
-                  //       ],
-                  //     ),
-                  //   ],
-                  // ),
-                  Container(
-                    width: 100.0,
-                    child: Table(
-                      //cho phép tuỳ biết width của column trong table,
-                      //tương ứng với từng cột được đánh só từ 0,1,2... và thứ tự từ bên phải sang.
-                      columnWidths: {
-                        1: FractionColumnWidth(0.5),
-                      },
-                      border: TableBorder.all(color: Colors.grey),
-                      children: [
-                        TableRow(children: [
-                          Container(
-                            width: 50.0,
-                            height: 50.0,
-                            child: MyButtonWidget(
-                              title: '3',
-                            ),
-                          ),
-                          Container(
-                            width: 50.0,
-                            height: 50.0,
-                            child: MyButtonWidget(
-                              title: '4',
-                            ),
-                          ),
-                        ]),
-                        TableRow(children: [
-                          Container(
-                              width: 50.0,
-                              height: 50.0,
-                              child: MyButtonWidget(
-                                title: '5',
-                              )),
-                          Container(
-                              width: 50.0,
-                              height: 50.0,
-                              child: MyButtonWidget(
-                                title: '6',
-                              )),
-                        ]),
-                      ],
-                    ),
+        body: Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+          ),
+          height: 300,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _controller1,
+                  child: Row(
+                    children: const [
+                      ItemFirtWidget(
+                        width: 100,
+                      ),
+                      ItemFirtWidget(
+                        bgColor: Colors.red,
+                      ),
+                      ItemFirtWidget(child: Icon(Icons.favorite)),
+                      ItemFirtWidget(
+                        bgColor: Colors.green,
+                      ),
+                      ItemFirtWidget(),
+                      ItemFirtWidget(),
+                      ItemFirtWidget(
+                        child: FittedBox(child: FittedBox(child: Text('a'))),
+                      ),
+                      ItemFirtWidget(),
+                      ItemFirtWidget(),
+                      ItemFirtWidget(
+                        right: true,
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _controller2,
+                  child: Row(
+                    children: const [
+                      ItemWidget(
+                        bgColor: Colors.blueGrey,
+                      ),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(
+                        right: true,
+                      )
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _controller3,
+                  child: Row(
+                    children: const [
+                      ItemWidget(),
+                      ItemWidget(
+                        bgColor: Colors.yellow,
+                      ),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(),
+                      ItemWidget(
+                        right: true,
+                      )
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _controller4,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(
+                            width: 100,
+                            height: 100,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(right: true),
+                          ItemWidget(
+                            right: true,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  controller: _controller5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: const [
+                          ItemWidget(
+                            height: 100,
+                            bgColor: Colors.blue,
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(),
+                          ItemWidget(),
+                        ],
+                      ),
+                      Column(
+                        children: const [
+                          ItemWidget(right: true),
+                          ItemWidget(
+                            right: true,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
-              width: 100.0,
-              child: Table(
-                border: TableBorder.all(color: Colors.grey),
-                columnWidths: const {
-                  1: FractionColumnWidth(.5),
-                },
-                children: const [
-                  TableRow(children: [
-                    TableRowItem(
-                      width: 50,
-                      height: 50,
-                      title: '7',
-                    ),
-                    TableRowItem(
-                      width: 50,
-                      height: 50,
-                      title: '8',
-                    ),
-                  ]),
-                  TableRow(children: [
-                    TableRowItem(width: 50, height: 100, title: '9'),
-                    TableRowItem(
-                      width: 50,
-                      height: 100,
-                      title: '10',
-                    ),
-                  ]),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
-    );
-  }
-}
-
-class TableRowItem extends StatelessWidget {
-  const TableRowItem({
-    this.width,
-    this.height,
-    required this.title,
-    Key? key,
-  }) : super(key: key);
-  final double? width;
-  final double? height;
-  final String title;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        width: width,
-        height: height,
-        child: MyButtonWidget(
-          title: title,
-        ));
-  }
-}
-
-class MyButtonWidget extends StatelessWidget {
-  MyButtonWidget({required this.title, this.textStyle});
-  final String title;
-  final TextStyle? textStyle;
-
-  @override
-  Widget build(BuildContext context) {
-    // từ context của MyButtonWidget sẽ tìm được widget Scaffold cha gần nhất
-    return FlatButton(
-      child: Center(child: FittedBox(child: Text(title))),
-      //color: Colors.pink,
-      onPressed: () {
-        final snackBar = SnackBar(
-          content: Text('Bạn đã chon $title'),
-          duration: Duration(milliseconds: 500),
-        );
-        Scaffold.of(context)
-            .showSnackBar(snackBar); // truyền vào context của MyButtonWidget
-      },
     );
   }
 }
